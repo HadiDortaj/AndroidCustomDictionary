92ca63be30e4410733cee88d7906c9c61ca6f284
Hadi-Dortaj <hadi.developer.m@gmail.com>
2020-07-25 22:04:22 +0430

    initial commit

---
+++ app/src/main/java/com/hadi/android/custom/dictionary/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hadi.android.custom.dictionary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
