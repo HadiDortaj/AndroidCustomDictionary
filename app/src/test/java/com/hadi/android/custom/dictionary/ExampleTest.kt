0e18ed755bc586efa54a4b2220f57fc90ad93a09
Hadi-Dortaj <hadi.developer.m@gmail.com>
2020-07-27 22:47:30 +0430

    switch to Junit5 and remove Junt4 dependencies

---
+++ app/src/test/java/com/hadi/android/custom/dictionary/ExampleTest.kt
@@ -0,0 +1,14 @@
+package com.hadi.android.custom.dictionary
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class ExampleTest {
+
+    @Test
+    fun test_2_is_equal_to_2() {
+        assertEquals(2,3)
+    }
+
+}
