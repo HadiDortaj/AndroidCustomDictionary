917ad81caa03e94f0cebb47085c1a94183e3f9d1
Hadi-Dortaj <hadi.developer.m@gmail.com>
2020-08-14 00:43:59 +0430

    add view model

---
+++ app/src/main/java/com/hadi/android/custom/dictionary/presenter/dialog/AddCategoryViewModel.kt
@@ -0,0 +1,13 @@
+package com.hadi.android.custom.dictionary.presenter.dialog
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class AddCategoryViewModel : ViewModel() {
+
+    val groupTitle : MutableLiveData<String> = MutableLiveData<String>("")
+
+    fun onBtnAddCategoryClick() {
+
+    }
+}
