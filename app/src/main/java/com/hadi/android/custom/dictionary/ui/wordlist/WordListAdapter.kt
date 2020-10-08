21d9fb4d7a3bcb071b999ceea930ad3f4b550023
Hadi-Dortaj <hadi.developer.m@gmail.com>
2020-10-09 01:19:36 +0330

    Add styled definition on each word item

--- app/src/main/java/com/hadi/android/custom/dictionary/ui/wordlist/WordListAdapter.kt
+++ app/src/main/java/com/hadi/android/custom/dictionary/ui/wordlist/WordListAdapter.kt
@@ -1,12 +1,18 @@
 package com.hadi.android.custom.dictionary.ui.wordlist
 
+import android.content.Context
+import android.graphics.Typeface
+import android.text.*
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.hadi.android.core.doman.Word
+import com.hadi.android.custom.dictionary.R
 import com.hadi.android.custom.dictionary.databinding.ItemWordBinding
-import java.lang.IllegalArgumentException
 
 class WordListAdapter(private val wordList: MutableList<Word>) :
     RecyclerView.Adapter<WordListAdapter.WordViewHolder>() {
@@ -39,8 +45,33 @@ class WordListAdapter(private val wordList: MutableList<Word>) :
     inner class WordViewHolder(val binding: ItemWordBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
+        val context: Context = binding.root.context
+
         fun bind(word: Word) {
             binding.word = word
+            binding.txtWordDefinition.text = getStyledDefinitionsText(word)
+        }
+
+        private fun getStyledDefinitionsText(word: Word): CharSequence? {
+            val spannable = SpannableStringBuilder("")
+            for (definition in word.definitions) {
+                val definitionType = definition.type
+                spannable.append(definitionType)
+                spannable.setSpan(
+                    ForegroundColorSpan(ContextCompat.getColor(context, R.color.colorPrimary)),
+                    (spannable.length - definitionType.length),
+                    spannable.length,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+                spannable.setSpan(
+                    StyleSpan(Typeface.BOLD_ITALIC),
+                    (spannable.length - definitionType.length),
+                    spannable.length,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+                spannable.append(" ${definition.definitionText} ")
+            }
+            return spannable
         }
     }
 
